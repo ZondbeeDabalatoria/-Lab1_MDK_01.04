bb40c67ab1d4e6e6e9782ed5cdb6007550bd6206
Ilya Vakhrushev <132203336+EliasVakhis@users.noreply.github.com>
2024-02-01 23:27:58 +0500

    Add files via upload

---
+++ Project_01/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Project_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(12, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(12, 282);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(12, 327);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 25);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(12, 362);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(70, 25);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(12, 401);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(82, 25);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(1014, 525);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(227, 25);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Цвет рабочего стола:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(1020, 643);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(454, 49);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Отправка данных";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(1020, 588);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(454, 49);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Задача цвета";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(1019, 554);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 29);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(1125, 553);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 29);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(1231, 553);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 29);
+            this.textBox3.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1497, 704);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Project_01/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics.SymbolStore;
+using System.Drawing.Design;
+
+namespace Project_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        struct OSVERSIONINFO
+        {
+            public uint dwOSVersionInfoSize;
+            public uint dwMajorVersion;
+            public uint dwMinorVersion;
+            public uint dwBuildNumber;
+            public uint dwPlatformId;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+            public string szCSDVersion;
+            public Int16 wServicePackMajor;
+            public Int16 wServicePackMinor;
+            public Int16 wSuiteMask;
+            public Byte wProductType;
+            public Byte wReserved;
+        }
+
+        public enum SystemMetric
+        {
+            SM_CXSCREEN = 0,  // 0x00
+            SM_CYSCREEN = 1,  // 0x01
+            SM_CXVSCROLL = 2,  // 0x02
+            SM_CYHSCROLL = 3,  // 0x03
+            SM_CYCAPTION = 4,  // 0x04
+            SM_CXBORDER = 5,  // 0x05
+            SM_CYBORDER = 6,  // 0x06
+            SM_CXDLGFRAME = 7,  // 0x07
+            SM_CXFIXEDFRAME = 7,  // 0x07
+            SM_CYDLGFRAME = 8,  // 0x08
+            SM_CYFIXEDFRAME = 8,  // 0x08
+            SM_CYVTHUMB = 9,  // 0x09
+            SM_CXHTHUMB = 10, // 0x0A
+            SM_CXICON = 11, // 0x0B
+            SM_CYICON = 12, // 0x0C
+            SM_CXCURSOR = 13, // 0x0D
+            SM_CYCURSOR = 14, // 0x0E
+            SM_CYMENU = 15, // 0x0F
+            SM_CXFULLSCREEN = 16, // 0x10
+            SM_CYFULLSCREEN = 17, // 0x11
+            SM_CYKANJIWINDOW = 18, // 0x12
+            SM_MOUSEPRESENT = 19, // 0x13
+            SM_CYVSCROLL = 20, // 0x14
+            SM_CXHSCROLL = 21, // 0x15
+            SM_DEBUG = 22, // 0x16
+            SM_SWAPBUTTON = 23, // 0x17
+            SM_CXMIN = 28, // 0x1C
+            SM_CYMIN = 29, // 0x1D
+            SM_CXSIZE = 30, // 0x1E
+            SM_CYSIZE = 31, // 0x1F
+            SM_CXSIZEFRAME = 32, // 0x20
+            SM_CXFRAME = 32, // 0x20
+            SM_CYSIZEFRAME = 33, // 0x21
+            SM_CYFRAME = 33, // 0x21
+            SM_CXMINTRACK = 34, // 0x22
+            SM_CYMINTRACK = 35, // 0x23
+            SM_CXDOUBLECLK = 36, // 0x24
+            SM_CYDOUBLECLK = 37, // 0x25
+            SM_CXICONSPACING = 38, // 0x26
+            SM_CYICONSPACING = 39, // 0x27
+            SM_MENUDROPALIGNMENT = 40, // 0x28
+            SM_PENWINDOWS = 41, // 0x29
+            SM_DBCSENABLED = 42, // 0x2A
+            SM_CMOUSEBUTTONS = 43, // 0x2B
+            SM_SECURE = 44, // 0x2C
+            SM_CXEDGE = 45, // 0x2D
+            SM_CYEDGE = 46, // 0x2E
+            SM_CXMINSPACING = 47, // 0x2F
+            SM_CYMINSPACING = 48, // 0x30
+            SM_CXSMICON = 49, // 0x31
+            SM_CYSMICON = 50, // 0x32
+            SM_CYSMCAPTION = 51, // 0x33
+            SM_CXSMSIZE = 52, // 0x34
+            SM_CYSMSIZE = 53, // 0x35
+            SM_CXMENUSIZE = 54, // 0x36
+            SM_CYMENUSIZE = 55, // 0x37
+            SM_ARRANGE = 56, // 0x38
+            SM_CXMINIMIZED = 57, // 0x39
+            SM_CYMINIMIZED = 58, // 0x3A
+            SM_CXMAXTRACK = 59, // 0x3B
+            SM_CYMAXTRACK = 60, // 0x3C
+            SM_CXMAXIMIZED = 61, // 0x3D
+            SM_CYMAXIMIZED = 62, // 0x3E
+            SM_NETWORK = 63, // 0x3F
+            SM_CLEANBOOT = 67, // 0x43
+            SM_CXDRAG = 68, // 0x44
+            SM_CYDRAG = 69, // 0x45
+            SM_SHOWSOUNDS = 70, // 0x46
+            SM_CXMENUCHECK = 71, // 0x47
+            SM_CYMENUCHECK = 72, // 0x48
+            SM_SLOWMACHINE = 73, // 0x49
+            SM_MIDEASTENABLED = 74, // 0x4A
+            SM_MOUSEWHEELPRESENT = 75, // 0x4B
+            SM_XVIRTUALSCREEN = 76, // 0x4C
+            SM_YVIRTUALSCREEN = 77, // 0x4D
+            SM_CXVIRTUALSCREEN = 78, // 0x4E
+            SM_CYVIRTUALSCREEN = 79, // 0x4F
+            SM_CMONITORS = 80, // 0x50
+            SM_SAMEDISPLAYFORMAT = 81, // 0x51
+            SM_IMMENABLED = 82, // 0x52
+            SM_CXFOCUSBORDER = 83, // 0x53
+            SM_CYFOCUSBORDER = 84, // 0x54
+            SM_TABLETPC = 86, // 0x56
+            SM_MEDIACENTER = 87, // 0x57
+            SM_STARTER = 88, // 0x58
+            SM_SERVERR2 = 89, // 0x59
+            SM_MOUSEHORIZONTALWHEELPRESENT = 91, // 0x5B
+            SM_CXPADDEDBORDER = 92, // 0x5C
+            SM_DIGITIZER = 94, // 0x5E
+            SM_MAXIMUMTOUCHES = 95, // 0x5F
+
+            SM_REMOTESESSION = 0x1000, // 0x1000
+            SM_SHUTTINGDOWN = 0x2000, // 0x2000
+            SM_REMOTECONTROL = 0x2001, // 0x2001
+
+
+            SM_CONVERTABLESLATEMODE = 0x2003,
+            SM_SYSTEMDOCKED = 0x2004,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct SYSTEMTIME
+        {
+            [MarshalAs(UnmanagedType.U2)] public short Year;
+            [MarshalAs(UnmanagedType.U2)] public short Month;
+            [MarshalAs(UnmanagedType.U2)] public short DayOfWeek;
+            [MarshalAs(UnmanagedType.U2)] public short Day;
+            [MarshalAs(UnmanagedType.U2)] public short Hour;
+            [MarshalAs(UnmanagedType.U2)] public short Minute;
+            [MarshalAs(UnmanagedType.U2)] public short Second;
+            [MarshalAs(UnmanagedType.U2)] public short Milliseconds;
+
+            public SYSTEMTIME(DateTime dt)
+            {
+                dt = dt.ToUniversalTime();  // SetSystemTime expects the SYSTEMTIME in UTC
+                Year = (short)dt.Year;
+                Month = (short)dt.Month;
+                DayOfWeek = (short)dt.DayOfWeek;
+                Day = (short)dt.Day;
+                Hour = (short)dt.Hour;
+                Minute = (short)dt.Minute;
+                Second = (short)dt.Second;
+                Milliseconds = (short)dt.Millisecond;
+            }
+        }
+
+        [DllImport("Kernel32")]
+        static extern unsafe bool GetComputerName(byte* lpBuffer, long* nSize);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        static extern bool GetUserName(System.Text.StringBuilder sb, ref Int32 length);
+
+        [DllImport("kernel32.dll")]
+        static extern uint GetSystemDirectory([Out] StringBuilder lpBuffer, uint uSize);
+
+        [DllImport("kernel32.dll")]
+        static extern bool GetVersionEx(ref OSVERSIONINFO osvi);
+        [DllImport("user32.dll")]
+        static extern int GetSystemMetrics(SystemMetric smIndex);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+
+        static extern bool SystemParametersInfo(uint uiAction, uint uiParam, StringBuilder pvParam, uint fWinIni);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SystemParametersInfo(uint uiAction, uint uiParam, ref uint pvParam, uint fWinIni);
+
+        [DllImport("user32.dll")]
+        static extern uint GetSysColor(int nIndex);
+
+        [DllImport("kernel32.dll")]
+        static extern void GetSystemTime(out SYSTEMTIME lpSystemTime);
+
+        [DllImport("user32.dll")]
+        static extern bool SetSysColors(int cElements, int[] lpaElements, int[] lpaRgbValues);
+
+        private unsafe void button1_Click(object sender, EventArgs e)
+        {
+            // Имя ПК
+            byte[] buffor = new byte[512];
+            long sizeNameComputer = buffor.Length;
+            long* pSize = &sizeNameComputer;
+            fixed (byte* pBuffor = buffor)
+            {
+                GetComputerName(pBuffor, pSize);
+            }
+
+            System.Text.Encoding textEnc = new System.Text.ASCIIEncoding();
+            label1.Text = $"Имя компа: {textEnc.GetString(buffor)}";
+
+            // Определение имени юзера
+            StringBuilder BufferUserName = new StringBuilder(512);
+            int sizeUserName = 512;
+            GetUserName(BufferUserName, ref sizeUserName);
+            label2.Text = $"Имя пользователя: {BufferUserName}";
+
+            // Системная директория
+            StringBuilder sbSystemDir = new StringBuilder(256);
+            uint sizePathSystemDir = 256;
+            GetSystemDirectory(sbSystemDir, sizePathSystemDir);
+            label3.Text = $"Системная директория: {sbSystemDir}";
+
+            // Вверсия ОС
+            OSVERSIONINFO osvi = new OSVERSIONINFO();
+            osvi.dwOSVersionInfoSize = (uint)Marshal.SizeOf(osvi);
+            GetVersionEx(ref osvi);
+            label4.Text = $"версия ОС: {osvi.dwBuildNumber}";
+
+            // Системные метрики
+            switch (GetSystemMetrics(SystemMetric.SM_CLEANBOOT))
+            {
+                case 0:
+                    label5.Text = "Тип загрузки: Normal boot";
+                    break;
+                case 1:
+                    label5.Text = "Тип загрузки: Fail safe boot";
+                    break;
+                case 2:
+                    label5.Text = "Тип загрузки: Fail safe boot with networking";
+                    break;
+            }
+
+            label6.Text = $"Разрешение монитора: {GetSystemMetrics(SystemMetric.SM_CXSCREEN)} x {GetSystemMetrics(SystemMetric.SM_CYSCREEN)}";
+
+            // берем системные настройки
+            StringBuilder pathWallpaper = new StringBuilder(1024);
+            const uint SPI_GETDESKWALLPAPER = 0x0073;
+            SystemParametersInfo(SPI_GETDESKWALLPAPER, 1024, pathWallpaper, 0);
+            label7.Text = $"Путь к картинке рабочего стола: {pathWallpaper}";
+
+            const uint SPI_GETFONTSMOOTHINGCONTRAST = 0x200C;
+            uint contrast = 0;
+            SystemParametersInfo(SPI_GETFONTSMOOTHINGCONTRAST, 0, ref contrast, 0);
+            label8.Text = $"Контрастность: {contrast}";
+
+            // Цвета
+            const int COLOR_BACKGROUND = 1;
+            int color1 = Convert.ToInt32(GetSysColor(COLOR_BACKGROUND));
+            Color color = Color.FromArgb(color1 & 0xFF, (color1 & 0xFF00) >> 8, (color1 & 0xFF0000) >> 16);
+            label9.Text = $"Цвет фона: R = {color.R} G = {color.G} B = {color.B}";
+
+            // Системное время
+            SYSTEMTIME mytime;
+            GetSystemTime(out mytime);
+            label10.Text = $"Дата: {mytime.Day}.{mytime.Month}.{mytime.Year}";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Фон рабочего стола
+                const int COLOR_DESKTOP = 1;
+                Color inColor = Color.FromArgb(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+                int[] elements = { COLOR_DESKTOP };
+                int[] colors = { System.Drawing.ColorTranslator.ToWin32(inColor) };
+                SetSysColors(elements.Length, elements, colors);
+            }
+            catch
+            {
+                MessageBox.Show("Максимум 255 в каждом канале!");
+            }
+        }
+    }
+}
+
